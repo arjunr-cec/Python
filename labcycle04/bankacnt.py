51871a842e20df2aa7060a39d186a91e91be76c6
arjunr-cec <146920200+arjunr-cec@users.noreply.github.com>
2023-12-13 10:04:22 +0530

    Update bankacnt.py

--- labcycle04/bankacnt.py
+++ labcycle04/bankacnt.py
@@ -1,39 +1,50 @@
-class bank:
-    def __init__(self,accno,name,accty,bal):
-        self.accno=accno
-        self.name=name
-        self.accty=accty
-        self.bal=0
+class Bank:
+    def __init__(self, accno, name, accty):
+        self.accno = accno
+        self.name = name
+        self.accty = accty
+        self.bal = 0
+
     def showamt(self):
-        print("account holderr name:",self.name)
-        print("account number:",self.accno)
-        print("account type:",self.accty)
-        print("your balance amount:",self.bal)
-    def depo(self,d1):
-        self.bal=self.bal+d1
+        print("Account holder name:", self.name)
+        print("Account number:", self.accno)
+        print("Account type:", self.accty)
+        print("Your balance amount:", self.bal)
+
+    def depo(self, d1):
+        self.bal = self.bal + d1
         return self.bal
-    def withd(self,w1):
-        self.bal=self.bal-w1
-        return self.bal
-    n=input("enter your name:")
-    a=int(input("enter your account number:"))
-    t=input("enter your account type:")
-    o=bank(a,n,t,bal0)
-    o.showamt()
-    while(True):
-        print("\n menu")
-        print("\n1.deposit")
-        print("\n2.withdraw")
-        c=int(input("enter yoyr choice:"))
-        if(c==1):
-            d=int(input("enter your choice:"))
-            print("your total balance is:",o.depo(d))
-        elif(c==2):
-            w=int(input("enter the amount to be withdrawed:"))
-            if(w>d):
-                print("INSUFFICIENT FUND")
-            else:
-                print("your total balance amount is",o.withd(w))
+
+    def withd(self, w1):
+        if w1 > self.bal:
+            print("INSUFFICIENT FUNDS")
         else:
-                print("enter a valid choice:")
-        
+            self.bal = self.bal - w1
+            return self.bal
+
+n = input("Enter your name:")
+a = int(input("Enter your account number:"))
+t = input("Enter your account type:")
+account = Bank(a, n, t)
+
+account.showamt()
+
+while True:
+    print("\n Menu")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Exit")
+
+    choice = int(input("Enter your choice:"))
+
+    if choice == 1:
+        deposit_amount = int(input("Enter the deposit amount:"))
+        print("Your total balance is:", account.depo(deposit_amount))
+    elif choice == 2:
+        withdraw_amount = int(input("Enter the amount to be withdrawn:"))
+        print("Your total balance amount is", account.withd(withdraw_amount))
+    elif choice == 3:
+        print("Exiting...")
+        break
+    else:
+        print("Enter a valid choice:")
